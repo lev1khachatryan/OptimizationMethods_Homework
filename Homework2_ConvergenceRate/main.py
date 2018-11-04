5d0b59b829bac5e490dac29ade8b5f35a17b6577
lev1khachatryan <levon.khachatryan.1996.db@gmail.com>
2018-11-04 22:36:15 +0400

    Homework 6 - code part : Rate of convergence

--- Homework2_ConvergenceRate/main.py
+++ Homework2_ConvergenceRate/main.py
@@ -23,6 +23,17 @@ def counter_c(n):
             current_elem += math.sqrt(6*(1/(j**2)))
         l1.append(abs(current_elem - math.pi))
     return l1
+def counter_d(n):
+    l1 = []
+    for i in range(1, n+1):
+        current_elem = 0.0
+        for j in range(1, i+1):
+            current_elem += (88*(-1)**j*(1/28)**(2*j+1))/(2*j+1) + 
+                            (8*(-1)**j*(1/443)**(2*j+1))/(2*j+1) - 
+                            (20*(-1)**j*(1/1393)**(2*j+1))/(2*j+1)-
+                            (40*(-1)**j*(1/11018)**(2*j+1))/(2*j+1)
+        l1.append(abs(current_elem - math.pi))
+    return l1
 def count_of_zeros(numbers):
     l1 = []
     for elem in numbers:
@@ -36,6 +47,7 @@ def count_of_zeros(numbers):
         l1.append(cnt)
     return(l1)
 n = 100
-print(count_of_zeros(counter_a(n)))
-print(count_of_zeros(counter_b(n)))
-print(count_of_zeros(counter_b(n)))
+count_of_zeros(counter_a(n))
+count_of_zeros(counter_b(n))
+count_of_zeros(counter_c(n))
+count_of_zeros(counter_d(n))
